3f53442e187884c494c3c713c618ae959b8fcb7d
avabrooks <72889462+avabrooks@users.noreply.github.com>
2021-03-18 13:00:43 -0700

    changed base.html to layout.html

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 #home page route
 @app.route('/')
 def home():
-    return render_template("base.html")
+    return render_template("layout.html")
 
 if __name__ == "__main__":
     app.run(port='3000', host='127.0.0.1')

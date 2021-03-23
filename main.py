02ce49c0303f02464a07b7995364d2dd67c58239
avabrooks <72889462+avabrooks@users.noreply.github.com>
2021-03-22 22:39:27 -0700

    change main route to home.html, blocks for pagecontent, rough gradient, starting database work

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# database setup
+dbURI = 'sqlite:///chess.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = dbURI
+db = SQLAlchemy(app)
+db.init_app(app)
+
+
 
 #home page route
 @app.route('/')
 def home():
-    return render_template("layout.html")
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(port='3000', host='127.0.0.1')

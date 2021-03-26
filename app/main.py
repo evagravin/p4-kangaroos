1ca7319b8d241376299c46ec1b791888ecf9b3a4
Risa <risaiwazaki>
2021-03-25 20:59:07 -0700

    custom file

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 import random
 
 from custom import apology, convert, convertList
+
 app = Flask(__name__)
 
 # database setup

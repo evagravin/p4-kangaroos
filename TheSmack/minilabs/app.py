f497bb92d354c05da31d57129e8743cfd94dd074
avabrooks <72889462+avabrooks@users.noreply.github.com>
2021-03-31 12:37:02 -0700

    rendering templates, start to minilab

--- TheSmack/minilabs/app.py
+++ TheSmack/minilabs/app.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 minilabMenu_bp = Blueprint('minilab Menu', __name__,
                            template_folder='templates',
@@ -26,7 +26,7 @@ def minilabMenu():
 
 @ava_minilab_bp.route('/')
 def ava_minilab():
-    return "Ava's mini lab page"
+    return render_template("/minilabs/ava-minilab.html")
 
 @risa_minilab_bp.route('/')
 def risa_minilab():
@@ -38,6 +38,6 @@ def linda_minilab():
 
 @eva_minilab_bp.route('/')
 def eva_minilab():
-    return "Eva's mini lab page"
+    return render_template("/minilabd/eva-minilab.html")
 
 
--- TheSmack/minilabs/ava-minilab.py
+++ TheSmack/minilabs/ava-minilab.py
@@ -1,8 +1,7 @@
-class tester:
-    def calc_series(self):
-        limit = self._series
-        f = [0, 1]  # fibonacci starting array/list
-        while limit > 0:
-            self.set_data(f[0])
-            f = [f[1], f[0] + f[1]]
-            limit -= 1
+class math:
+    def Average(lst):
+        return sum(lst) / len(lst)
+
+    lst = [15, 9, 55, 41, 35, 20, 62, 49]
+    average = Average(lst)
+    print("Average of the list =", average)

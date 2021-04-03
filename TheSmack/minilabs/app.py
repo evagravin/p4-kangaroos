bc03789d8fc7febbf2b0827f8e440dba7b14a3d4
Risa <risaiwazaki>
2021-04-02 23:32:45 -0700

    minilab pt2

--- TheSmack/minilabs/app.py
+++ TheSmack/minilabs/app.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template, request
 from TheSmack.minilabs.ava_minilab import math
 from TheSmack.minilabs.eva_minilab import Foil
 from TheSmack.minilabs.risa_minilab import Calc1
+from TheSmack.minilabs.risa_minilab import Calc2
+from TheSmack.minilabs.risa_minilab import Calc3
 
 
 
@@ -39,8 +41,14 @@ def risa_minilab():
         temp1 = int(request.form['temp1'])
         calc1 = Calc1(temp1)
         print(f"{calc1.answer1}")
-        return render_template("/minilabs/risa-minilab.html", calc1=calc1)
-    return render_template("/minilabs/risa-minilab.html", calc1=Calc1 )
+        weight2 = int(request.form['weight2'])
+        calc2 = Calc2(weight2)
+        print(f"{calc2.answer2}")
+        length3 = int(request.form['length3'])
+        calc3 = Calc3(length3)
+        print(f"{calc3.answer3}")
+        return render_template("/minilabs/risa-minilab.html", calc1=calc1, calc2=calc2, calc3=Calc3)
+    return render_template("/minilabs/risa-minilab.html", calc1=Calc1, calc2=Calc2, calc3=Calc3 )
 
 
 

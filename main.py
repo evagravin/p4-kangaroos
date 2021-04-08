9f3ec87a2313531264279404ba6719d8f5d1d470
Risa <risaiwazaki>
2021-04-07 20:45:12 -0700

    main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from TheSmack.users.app import usermenu_bp, signup_bp, login_bp, profile_bp, success_bp
+#from TheSmack.users.app import usermenu_bp, signup_bp, login_bp, profile_bp, success_bp
 from TheSmack.social_media.app import trending_bp, createSmack_bp, smackDM_bp, searchresults_bp, aboutus_bp, smackmenu_bp
 from TheSmack.minilabs.app import minilab_bp
 
@@ -16,18 +16,18 @@ db.init_app(app)
 
 
 app = Flask(__name__)
-app.register_blueprint(usermenu_bp, url_prefix='/usermenu')
-app.register_blueprint(signup_bp, url_prefix='/signup')
-app.register_blueprint(login_bp, url_prefix='/login')
+#app.register_blueprint(usermenu_bp, url_prefix='/usermenu')
+#app.register_blueprint(signup_bp, url_prefix='/signup')
+#app.register_blueprint(login_bp, url_prefix='/login')
 app.register_blueprint(trending_bp, url_prefix='/trending')
 app.register_blueprint(createSmack_bp, url_prefix='/createsmack')
 app.register_blueprint(smackDM_bp, url_prefix='/smackDM')
-app.register_blueprint(profile_bp, url_prefix='/profile')
+#app.register_blueprint(profile_bp, url_prefix='/profile')
 app.register_blueprint(searchresults_bp, url_prefix='/searchresults')
 app.register_blueprint(minilab_bp, url_prefix='/minilab')
 app.register_blueprint(aboutus_bp, url_prefix='/aboutus')
 app.register_blueprint(smackmenu_bp, url_prefix='/smackmenu')
-app.register_blueprint(success_bp, url_prefix='/success')
+#app.register_blueprint(success_bp, url_prefix='/success')
 
 #home page route
 

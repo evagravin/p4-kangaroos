e5faf66ff99870fab096f82e6a45c0e1bc7b4bcc
lindalonglong <lindal49692@gmail.com>
2021-05-07 13:52:32 -0700

    bubblesort py

--- TheSmack/bubblesort/linda_bubblesort.py
+++ TheSmack/bubblesort/linda_bubblesort.py
@@ -8,18 +8,27 @@ class linda:
             self._lst.append(int(value))
 
 
-    def sortingbubble(self):
-        length = len(self._lst) - 1
-        unsorted = True
-    
-        while unsorted:
-            for element in range(0,length):
-                unsorted = False
-                if self._lst[element] > self._lst[element + 1]:
-                    hold = self._lst[element + 1]
-                    self._lst[element + 1] = self._lst[element]
-                    self._lst[element] = hold
-                else:
-                    unsorted = True
+    # An optimized version of Bubble Sort
+    def sortingbubble(arr):
+        n = len(arr)
 
+        # Traverse through all array elements
+        for i in range(n):
+            swapped = False
 
+            # Last i elements are already
+            #  in place
+            for j in range(0, n-i-1):
+
+                # traverse the array from 0 to
+                # n-i-1. Swap if the element
+                # found is greater than the
+                # next element
+                if arr[j] > arr[j+1] :
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+                    swapped = True
+
+            # IF no two elements were swapped
+            # by inner loop, then break
+            if swapped == False:
+                break

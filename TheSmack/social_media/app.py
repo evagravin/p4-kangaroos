dad317adc745298ca52b338e35272297bbc49dec
avabrooks <72889462+avabrooks@users.noreply.github.com>
2021-05-19 12:56:37 -0700

    login and logout

--- TheSmack/social_media/app.py
+++ TheSmack/social_media/app.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, app, redirect, url_for
+from flask_login import LoginManager,login_required, current_user, logout_user
 from TheSmack.social_media.post import post_create
+from TheSmack.users.user import User
+
 
 
 trending_bp = Blueprint('trending', __name__,
@@ -25,6 +28,12 @@ smackmenu_bp = Blueprint('smackmenu', __name__,
                          template_folder='templates',
                          static_folder='static')
 
+
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home.html'))
+
 @aboutus_bp.route('/')
 def aboutus():
     return render_template ("digitalPortfolios.html")
@@ -43,7 +52,7 @@ def results():
     return "Search results page"
 
 @createSmack_bp.route('/', methods=['POST', 'GET'])
-#@login_required
+@login_required
 def createSmack():
     if request.method == 'POST':
         emotion = request.form['emotion']
